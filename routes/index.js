8fa1b9c4a761b70d408f4c3ec4d066ba4d19d3c5
André Pereira da Silva <andre.fillype@vortal.biz>
2020-06-30 12:23:47 +0100

    Changing version app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({ title: "My Movies Server", version: "1.0.3" });
+    res.status(200).send({ title: "My Movies Server", version: "1.0.5" });
 });
 
 module.exports = router;
